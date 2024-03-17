195d53208d296e55c6d07bb01565a072060e17e6
bizip <bizip04@gmail.com>
2024-03-17 19:47:23 +0200

    Add a create new opportunity endpoint

--- src/restful/controllers/OpportunitiesControllers.js
+++ src/restful/controllers/OpportunitiesControllers.js
@@ -1,6 +1,7 @@
 /* eslint-disable quotes */
 import dotenv from "dotenv";
 import Util from "../../helper/utils";
+const { v4: uuidv4 } = require("uuid");
 const admin = require("firebase-admin");
 
 dotenv.config();
@@ -16,6 +17,59 @@ class OpportunitiesController {
    * @param {Object} res response Object.
    * @returns {Object} response Object.
    */
+  static async newOpportunities(req, res) {
+    const { title, description, type, deadline, budget } = req.body;
+    const db = admin.firestore();
+    const documentId = "open";
+    const newOpportunityId = uuidv4();
+    const newData = {
+      title,
+      description,
+      type,
+      deadline,
+      budget,
+      opportunity_id: newOpportunityId,
+      user_id: req.user.user_id,
+      createdAt: new Date().toISOString().substring(0, 19),
+      updatedAt: new Date().toISOString().substring(0, 19),
+    };
+
+    db.collection("opportunities")
+      .doc(documentId)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          if (
+            data.data.some((item) => item.opportunity_id === newOpportunityId)
+          ) {
+            util.statusCode = 409;
+            util.message = `Opportunity with ID '${newOpportunityId}' already exists.`;
+            return util.send(res);
+          }
+        }
+        db.collection("opportunities")
+          .doc(documentId)
+          .update({
+            data: admin.firestore.FieldValue.arrayUnion(newData),
+          })
+          .then(() => {
+            util.statusCode = 200;
+            util.message = "New opportunity created successfully";
+            return util.send(res);
+          })
+          .catch((error) => {
+            util.statusCode = 500;
+            util.message = error.message || "Server error";
+            return util.send(res);
+          });
+      })
+      .catch((error) => {
+        util.statusCode = 500;
+        util.message = error.message || "Server error";
+        return util.send(res);
+      });
+  }
 
   static async getAllOpportunities(req, res) {
     // const { uid } = req.user;
@@ -32,8 +86,6 @@ class OpportunitiesController {
       });
 
       if (opportunities.length > 0) {
-        // console.log("opportunities found:", opportunities);
-        // console.log("Total number of opportunities:", opportunities.length);
         util.statusCode = 200;
         util.message = opportunities;
         return util.send(res);
--- src/restful/routes/opportunitiesRouters.js
+++ src/restful/routes/opportunitiesRouters.js
@@ -6,5 +6,6 @@ import OpportunitiesController from "../controllers/OpportunitiesControllers";
 const router = Router();
 
 router.get("/",protect, OpportunitiesController.getAllOpportunities);
+router.post("/",protect, OpportunitiesController.newOpportunities);
 
 module.exports = { router };

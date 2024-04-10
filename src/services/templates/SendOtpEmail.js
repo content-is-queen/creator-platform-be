68e36b6cfe0d8a069722a85fd11daf1a62b0adfb
bizip <bizip04@gmail.com>
2024-04-10 20:50:36 +0200

    Style the email template

--- src/services/templates/SendOtpEmail.js
+++ src/services/templates/SendOtpEmail.js
@@ -5,58 +5,73 @@ dotenv.config();
 export const sendOtpEmail = (emailData) => {
   const link = `${emailData.otp}`;
   const template = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-        <title>Document</title>
-        <style>
-        *{
-            box-sizing: border-box;
-            margin:0px;
-            padding:0px;
-        }
-        html,body{
-            font-family: poppins;
-        }
-        .logo{
-            width:100%;
-            height:auto;
-            background:#f2f2f2;
-            padding:5px 0px 5px 10px;
-            
-        }
-        .logo > div{
-            font-size:20px;
-        }
-        .content{
-            padding:10px;
-            color:#121111;
-        }
-        .button{
-            background-color: rgb(54 103 237);
-            padding:13px;
-            border:0px;
-            font-size:15px;
-            margin-top:28px;
-            text-decoration: none;
-            color:#121111
-        }
-    </style>
-    </head>
-    <body>
-        <div class="wrapper">
-           <div class="logo">
-           </div>
-          <center> <div class="content">
-            <p>Hello ${emailData.name} ,Thank you for registering creator platform</p>
-            <p>To Finish your registration use this otp <b>${link}</b> </p>
-            <br>
-        Thank you
-        </div></center>
-        </div>
-    </body>
-    </html>`;
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+      <title>Creator Platform</title>
+      <style>
+          * {
+              box-sizing: border-box;
+              margin: 0;
+              padding: 0;
+          }
+          body {
+              font-family: 'Poppins', sans-serif;
+              background-color: #f2f2f2;
+          }
+          .container {
+              max-width: 600px;
+              margin: 0 auto;
+              background-color: #ffffff;
+              border-radius: 10px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+          .logo {
+              text-align: center;
+          }
+          .logo img {
+              width: 100%;
+              height: auto;
+          }
+          .content {
+              color: #121111;
+             padding: 20px;
+          }
+          .button {
+              display: inline-block;
+              background-color: #3677ed;
+              color: #ffffff;
+              padding: 10px 20px;
+              border-radius: 5px;
+              text-decoration: none;
+              margin-top: 20px;
+          }
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <div class="logo">
+              <img src="https://media.licdn.com/dms/image/D4E3DAQFc-hrExVgIUg/image-scale_191_1128/0/1707842351543/content_is_queen_podcasts_cover?e=2147483647&v=beta&t=etcyrsZcy5eBtGjVp18vW4aQXkHiNWU0rBY357UqjQg" alt="Logo">
+          </div>
+          <div class="content">
+              <p>Hello ${emailData.name},</p>
+              <p> we kindly request you to verify your email address.
+  
+  Please use the following OTP (One-Time Password) to verify your email:.</p>
+              <p>To finish your registration, please use this OTP:</p>
+          </div>
+          <div class="button-container" style="text-align: center;">
+              <a href="#" class="button"><b>${link}</b></a>
+          </div>
+          <div class="content">
+              <p>Thank you for choosing Creator Platform. We look forward to having you as part of our community.</p>
+            <p>Best regards</p>
+              <p>Creator Platform Team</p>
+          </div>
+      </div>
+  </body>
+  </html>`;
   return template;
 };

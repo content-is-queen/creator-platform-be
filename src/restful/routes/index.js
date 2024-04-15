f25f19c50d49829988cf8e3569e8ca85863b91c8
Kaleshe <kalesheav@gmail.com>
2024-04-15 11:30:43 +0100

    fix: merge error

--- src/restful/routes/index.js
+++ src/restful/routes/index.js
@@ -3,22 +3,15 @@ const { Router } = require("express");
 const { authRouter } = require("./authRouter");
 const { chatRouter } = require("./chatRouter");
 const { opportunitiesRouter } = require("./opportunitiesRouter");
-const {applicationsRouter } = require("./applicationsRouter")
+const { applicationsRouter } = require("./applicationsRouter");
 
 const API_VERSION = process.env.API_VERSION || "v1";
 const router = Router();
 
-<<<<<<< HEAD
-router.use(`${url}/auth`, authRouter);
-router.use(`${url}/messages`, messagesRouter);
-router.use(`${url}/opportunities`, opportunitiesRouter);
-router.use(`${url}/applications`, applicationsRouter);
-=======
 router.use(`/${API_VERSION}/auth`, authRouter);
 router.use(`/${API_VERSION}/messages`, chatRouter);
 router.use(`/${API_VERSION}/opportunities`, opportunitiesRouter);
-router.use(`${API_VERSION}/applications`, applicationsRouter);
->>>>>>> main
+router.use(`/${API_VERSION}/applications`, applicationsRouter);
 
 router.all(`/${API_VERSION}/`, (req, res) => {
   return res

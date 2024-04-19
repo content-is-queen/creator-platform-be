8dff6dd318cd14c7a38421df28feb8343a71c71f
toyeAkinoso <toye.akinoso@yahoo.com>
2024-04-19 14:04:18 +0100

    added new routes

--- src/restful/routes/authRouter.js
+++ src/restful/routes/authRouter.js
@@ -9,4 +9,8 @@ router.post("/signup/brand", AuthController.signupBrand);
 router.post("/signup/creator", AuthController.signupCreator);
 router.patch("/profile", protect, AuthController.updateProfile);
 
+// New route for fetching non-sensitive user profile data
+router.get("/profile/non-sensitive/:role/:userId", AuthController.getNonSensitiveProfile);
+
+
 module.exports.authRouter = router;

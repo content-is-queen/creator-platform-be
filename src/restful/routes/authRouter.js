742af6ac28b003a61629ff99360a6a04b5b13ee7
Kaleshe <kalesheav@gmail.com>
2024-04-23 15:33:09 +0100

    tweak: reorganise auth router

--- src/restful/routes/authRouter.js
+++ src/restful/routes/authRouter.js
@@ -5,13 +5,13 @@ const { protect } = require("../../middlewares");
 const router = Router();
 
 router.get("/user", protect, AuthController.getUser);
+router.patch("/user", protect, AuthController.updateUser);
+
 router.get("/users", AuthController.getAllUsers);
 router.post("/signup", AuthController.signup);
-router.patch("/user", protect, AuthController.updateUser);
 router.post("/verify", AuthController.verifyOtp);
 router.post("/reset", AuthController.resetUserPassword);
 
-// New route for fetching non-sensitive user profile data
 router.get("/user/:user_id", AuthController.getPublicUser);
 
 module.exports.authRouter = router;

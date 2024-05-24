1f938668f4d35ef4f81183ff04b7e401fcce93d6
Mundurujoana <mundujoana@com>
2024-05-24 17:09:00 +0200

    deployment

--- src/index.js
+++ src/index.js
@@ -6,30 +6,29 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const fileUploader = require("express-fileupload");
 const router = require("./restful/routes");
 const apiLogger = require("./helper/apiCallLog");
+const serviceAccount = {
+  "type": process.env.SERVICE_ACCOUNT_TYPE,
+  "project_id": process.env.PROJECT_ID, // Corrected
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+};
 
 dotenv.config();
-const PORT = process.env.PORT || 5000; // Use environment variable for port or default to 5000
+const PORT = 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Initialize Firebase app with service account credentials
 initializeApp({
-  credential: cert({
-    "type": process.env.SERVICE_ACCOUNT_TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
-  }),
+  credential: cert(serviceAccount),
   storageBucket: "contentisqueen-97ae5.appspot.com",
 });
-
 app.use(apiLogger);
 app.use(
   fileUploader({
@@ -40,7 +39,6 @@ app.use(
 );
 
 app.use(router);
-
 const start = () => {
   try {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`));

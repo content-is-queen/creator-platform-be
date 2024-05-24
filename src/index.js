127309761a3f2db4de992bbddb39804bd5819b52
Mundurujoana <mundujoana@com>
2024-05-24 15:20:25 +0200

    Prepare for hosting API somewhere low cost

--- src/index.js
+++ src/index.js
@@ -4,19 +4,32 @@ const cors = require("cors");
 const functions = require("firebase-functions");
 const { initializeApp, cert } = require("firebase-admin/app");
 const fileUploader = require("express-fileupload");
-const serviceAccount = require("../contentisqueen-97ae5-firebase-adminsdk-qhkbo-6886ee17eb.json");
 const router = require("./restful/routes");
 const apiLogger = require("./helper/apiCallLog");
 
 dotenv.config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000; // Use environment variable for port or default to 5000
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Initialize Firebase app with service account credentials
 initializeApp({
-  credential: cert(serviceAccount),
+  credential: cert({
+    "type": process.env.SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+  }),
   storageBucket: "contentisqueen-97ae5.appspot.com",
 });
+
 app.use(apiLogger);
 app.use(
   fileUploader({
@@ -27,6 +40,7 @@ app.use(
 );
 
 app.use(router);
+
 const start = () => {
   try {
     app.listen(PORT, () => console.log(`App listening on port ${PORT}`));

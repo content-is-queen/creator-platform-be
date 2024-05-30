0742af1af988ba3956ce020da30e059ab09f603c
toyeAkinoso <toye.akinoso@yahoo.com>
2024-05-30 11:27:53 +0100

    updated

--- src/restful/controllers/paymentController.js
+++ src/restful/controllers/paymentController.js
@@ -1,4 +1,6 @@
 // controllers/paymentController.js
+
+
 const stripe = require('stripe')('sk_test_51PEYUjA0tTttcwfyvnIjHg1BxxxYr1pNtClrkA2UBhfWV6wBW3xcpjMgGEyl4VjrlDH9Xnsf2oz7VWnIoMW2ylba00GKhqrB3g'); // Make sure to set your Stripe secret key in your environment variables
 
 /**
@@ -32,108 +34,6 @@ const createCheckoutSession = async (req, res) => {
 };
 
 
-const createSubscription = async (req, res) => {
-    const { email, paymentMethodId, priceId } = req.body;
 
-    try {
-        // Check if the customer already exists
-        let customer = await getOrCreateCustomer(email, paymentMethodId);
-
-        // Attach the payment method to the customer if not already attached
-        await stripe.paymentMethods.attach(paymentMethodId, {
-            customer: customer.id,
-        });
-
-        // Update customer's default payment method
-        await stripe.customers.update(customer.id, {
-            invoice_settings: {
-                default_payment_method: paymentMethodId,
-            },
-        });
-
-        // Create the subscription
-        const subscription = await stripe.subscriptions.create({
-            customer: customer.id,
-            items: [{ price: priceId }],
-            expand: ['latest_invoice.payment_intent'],
-        });
-
-        // Respond with the subscription details
-        res.status(200).json(subscription);
-    } catch (error) {
-        console.error('Error creating subscription:', error);
-        res.status(400).json({ error: { message: error.message } });
-    }
-};
-
-/**
- * Helper function to get or create a customer.
- * @param {string} email - Customer email.
- * @param {string} paymentMethodId - Payment method ID.
- * @returns {object} - Customer object.
- */
-const getOrCreateCustomer = async (email, paymentMethodId) => {
-    // Check if customer already exists with the provided email
-    const existingCustomers = await stripe.customers.list({ email });
-    if (existingCustomers.data.length > 0) {
-        return existingCustomers.data[0];
-    } else {
-        // Create a new customer if not found
-        return await stripe.customers.create({
-            email,
-            payment_method: paymentMethodId,
-            invoice_settings: {
-                default_payment_method: paymentMethodId,
-            },
-        });
-    }
-};
-
-/**
- * Create a customer.
- * @param {Request} req - Express request object.
- * @param {Response} res - Express response object.
- */
-const createCustomer = async (req, res) => {
-    try {
-        const customer = await stripe.customers.create({
-            email: req.body.email,
-        });
-        res.status(200).json({ customer });
-    } catch (error) {
-        console.error('Error creating customer:', error);
-        res.status(500).json({ error: { message: 'An error occurred while creating the customer.' } });
-    }
-};
-
-/**
- * Create a payment method.
- * @param {Request} req - Express request object.
- * @param {Response} res - Express response object.
- */
-const createPaymentMethod = async (req, res) => {
-    const { cardNumber, expMonth, expYear, cvc } = req.body;
-
-    try {
-        const paymentMethod = await stripe.paymentMethods.create({
-            type: 'card',
-            card: {
-                number: cardNumber,
-                exp_month: expMonth,
-                exp_year: expYear,
-                cvc: cvc,
-            },
-        });
-        res.status(200).json({ paymentMethod });
-    } catch (error) {
-        console.error('Error creating payment method:', error);
-        res.status(500).json({ error: { message: 'An error occurred while creating the payment method.' } });
-    }
-};
-
-module.exports = {
-    createSubscription,
-    createCustomer,
-    createPaymentMethod,
-    createCheckoutSession
+module.exports = {createCheckoutSession
 };
--- src/restful/routes/paymentRouter.js
+++ src/restful/routes/paymentRouter.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { createSubscription, createCustomer,createPaymentMethod, createCheckoutSession } = require('../controllers/paymentController');
+const {createCheckoutSession } = require('../controllers/paymentController');
 
 const router = express.Router();
 
-router.post('/create-payment-intent', createSubscription);
-router.post('/create-customer', createCustomer);
 
-router.post('/create-payment-method', createPaymentMethod);
 router.post('/create-checkout-session', createCheckoutSession);
 
 module.exports.paymentsRouter = router;

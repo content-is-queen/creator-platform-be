f382a43dea4c2232cc63033dac9f1451f4aba6d0
toyeAkinoso <toye.akinoso@yahoo.com>
2024-06-05 11:07:57 +0100

    updated feat/plus backend

--- src/restful/controllers/authController.js
+++ src/restful/controllers/authController.js
@@ -439,7 +439,6 @@ class AuthController {
   }
 
   static async updateUserSubscription(req, res) {
-    console.log("this is being called update users");
     try {
       const { subscribed } = req.body;
       const { user_id } = req.params; // Assuming you have access to the user's ID
@@ -453,6 +452,40 @@ class AuthController {
       return util.send(res);
     } catch (error) {
       console.error("Error updating user subscribed status:", error);
+      util.statusCode = 500;
+      util.message = error.message || "Server error";
+      return util.send(res);
+    }
+  }
+
+  static async checkSubscription(req, res) {
+    try {
+      const { user_id } = req.params;
+
+      if (!user_id) {
+        util.statusCode = 400;
+        util.message = "User ID is required";
+        return util.send(res);
+      }
+
+      const db = admin.firestore();
+      const docRef = db.collection("users").doc(user_id);
+      const docSnapshot = await docRef.get();
+
+      if (!docSnapshot.exists) {
+        util.statusCode = 404;
+        util.message = "User not found";
+        return util.send(res);
+      }
+
+      const userData = docSnapshot.data();
+      const isSubscribed = userData.subscribed || false;
+
+      util.statusCode = 200;
+      util.message = { subscribed: isSubscribed };
+      return util.send(res);
+    } catch (error) {
+      console.error("Error checking subscription:", error);
       util.statusCode = 500;
       util.message = error.message || "Server error";
       return util.send(res);

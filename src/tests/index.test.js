bb3946ba51f258d90ee9da04d4bab045a1e28bb4
bizip <bizip04@gmail.com>
2024-06-17 11:25:44 +0200

    Ass test for src/tests/allowedRole.test.js

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,11 +1,10 @@
-import { describe, it } from "mocha";
+const { describe, it } = require("mocha");
 
 describe("Simple Math Test", () => {
-    it("should return 2 when adding 1 + 1", () => {
-      const sum = 1 + 1;
-      if (sum !== 2) {
-        throw new Error(`Expected 1 + 1 to be 2, but got ${sum}`);
-      }
-    });
+  it("should return 2 when adding 1 + 1", () => {
+    const sum = 1 + 1;
+    if (sum !== 2) {
+      throw new Error(`Expected 1 + 1 to be 2, but got ${sum}`);
+    }
   });
-  
+});

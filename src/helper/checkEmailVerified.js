19799719696ec7d26493be204d88cf89c4e9ff34
bizip <bizip04@gmail.com>
2024-07-17 10:26:36 +0200

    Fix prettier issue

--- src/helper/checkEmailVerified.js
+++ src/helper/checkEmailVerified.js
@@ -1,18 +1,17 @@
 const admin = require("firebase-admin");
 
 const checkEmailVerified = async (uid) => {
-    if (!uid) {
-      throw new Error('User ID (uid) is required');
-    }
-  
-    try {
-      const userRecord = await admin.auth().getUser(uid);
-      return userRecord.emailVerified;
-    } catch (error) {
-      console.error('Error checking email verification status:', error);
-      throw new Error('Error checking email verification status');
-    }
-  };
-  
-  module.exports = checkEmailVerified;
-  
+  if (!uid) {
+    throw new Error("User ID (uid) is required");
+  }
+
+  try {
+    const userRecord = await admin.auth().getUser(uid);
+    return userRecord.emailVerified;
+  } catch (error) {
+    console.error("Error checking email verification status:", error);
+    throw new Error("Error checking email verification status");
+  }
+};
+
+module.exports = checkEmailVerified;
